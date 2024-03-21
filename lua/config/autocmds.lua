b5a1b6c264b6c647f7fdf218392301125f966941
Hector Dufau <contato@hectordufau.com.br>
2024-03-21 16:47:55 -0300

    first commit

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,44 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = augroup("autoupdate"),
+  callback = function()
+    if require("lazy.status").has_updates then
+      require("lazy").update({ show = false })
+    end
+  end,
+})
+
+local augroupblade = vim.api.nvim_create_augroup("lsp_blade_workaround", { clear = true })
+
+-- Autocommand to temporarily change 'blade' filetype to 'php' when opening for LSP server activation
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroupblade,
+  pattern = "*.blade.php",
+  callback = function()
+    vim.bo.filetype = "php"
+  end,
+})
+
+-- Additional autocommand to switch back to 'blade' after LSP has attached
+vim.api.nvim_create_autocmd("LspAttach", {
+  pattern = "*.blade.php",
+  callback = function(args)
+    vim.schedule(function()
+      -- Check if the attached client is 'intelephense'
+      for _, client in ipairs(vim.lsp.get_clients()) do
+        if client.name == "intelephense" and client.attached_buffers[args.buf] then
+          vim.api.nvim_buf_set_var(args.buf, "filetype", "blade")
+          -- update treesitter parser to blade
+          vim.api.nvim_buf_set_var(args.buf, "syntax", "blade")
+          break
+        end
+      end
+    end)
+  end,
+})
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
+-- [[
+-- return {
+--  {
+--    "sainnhe/sonokai",
+--    opt = {
+--      style = "default",
+--      transparent_background = 1,
+--      better_performance = 1,
+--    },
+--  },
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "sonokai",
+--    },
+--  },
+--}
+-- ]]
