48a52464519acb513f4988d3045baa303bf10b8c
Hector Dufau <contato@hectordufau.com.br>
2024-03-22 16:26:53 -0300

    config update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -21,33 +21,3 @@ return {
     },
   },
 }
---
--- return {
---  {
---    "sainnhe/sonokai",
---    opt = {
---      style = "default",
---      transparent_background = 1,
---      better_performance = 1,
---    },
---  },
---  {
---    "LazyVim/LazyVim",
---    opts = {
---      colorscheme = "sonokai",
---    },
---  },
---}
---
-
--- {
---    "folke/tokyonight.nvim",
---    opts = {
---      transparent = true,
---      styles = {
---        sidebars = "transparent",
---        floats = "transparent",
---      },
---    },
---  },
---     colorscheme = "tokyonight-night",
