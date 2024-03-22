3bc07d5d8aee8736696fbaf3762107b78592c460
Hector Dufau <contato@hectordufau.com.br>
2024-03-22 12:59:12 -0300

    lazim update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -42,3 +42,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end)
   end,
 })
+
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
